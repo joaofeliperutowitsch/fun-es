8a56d1531f424872945d5409e94f695fc483ca8e
joaofeliperutowitsch <jfrutowitsch@gmail.com>
2022-06-19 19:22:13 -0300

    1 - create a function that displays a message on the console 2 - create a function that receives its name as (parameter) and displays it on the console 3 - create a function that takes its name, age, and a preferred musical style (parameters) and displays on console 4 - create a function that receives a movie, a song (parameters) and displays it on the console

---
+++ script.js
@@ -0,0 +1,23 @@
+function mensagem(){
+    console.log("Agora você consegue mandar uma mensagem!");
+}
+mensagem()
+
+
+function nome (nome) {
+    console.log (nome);
+}
+nome ("João Felipe")
+  
+
+function sobre (nome, idade, estilomusical){
+    console.log (`Meu nome é ${ nome } , tenho ${ idade } anos e meu estilo musical favorito é ${estilomusical}!`)
+}
+
+sobre ("João", "16", "Rock")
+
+function musicafilme (filmes, musica){
+    console.log (`Minha saga de filme preferido ${filmes}. Gosto muito de ouvir a música ${musica}`)
+}
+
+musicafilme ("Velozes & Furiosos", "Wish You Were Here")
